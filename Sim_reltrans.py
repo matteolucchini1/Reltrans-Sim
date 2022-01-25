9ef33d9c80a3c414ade16e9f5e54bff5c14a3061
Matteo Lucchini <matteol@mit.edu>
2022-01-25 14:57:25 -0500

    Minor tweak to files

--- Sim_reltrans.py
+++ Sim_reltrans.py
@@ -18,12 +18,10 @@ plt.rcParams.update({'font.size': 18})
 
 #Initialize convenient things:
 parameters_dbl = ['h1','h2','a','inc','rin','rout','zcos','Gamma','logxi','Afe','lognep','kTe','lumratio','nH','boost','Mass',
-                  'floHz','fhiHz','ReIm','DelA','DelAB','g','RESP','norm']
-parameters_dcp = ['h1','a','inc','rin','rout','zcos','Gamma','logxi','Afe','lognep','kTe','nH','boost','Mass','floHz','fhiHz',
-                  'ReIm','DelA','DelAB','g','RESP','norm']
-parameters_dist = ['h1','a','inc','rin','rout','zcos','Gamma','dist','Afe','lognep','kTe','nH','qboost','Mass','honr','b1','b2',
-                   'ReIm','floHz','fhiHz','DelA','DelAB','g','RESP','norm']
-lags_sim = ['gammac','Texp','pow']
+                  'floHz','fhiHz','gammac','DelA','DelAB','g','Texp','pow','RESP']
+
+lagonly_dbl = [16,17,18,19,20,21,23]
+
 cyndaquil = ['#ff5a00','#ff9c00','#ffde00','#ffeea4','#e6cd6a','#c5a46a','#7b6220','#18394a','#39627b','#62839c','#acacac']
 
 mod_color = cyndaquil[0]
@@ -46,55 +44,54 @@ while (len(sim_mode) == 0):
         sim_mode = ""
 #ask for input
 if(sim_mode == "value"):
-    print("Input simulation parameters for the time-averaged spectrum:")
+    print("Input simulation parameters for every spectrum:")
     sim_pars = np.zeros(len(parameters_dbl))
     for i in range(len(parameters_dbl)):
-        if (i<16 or i>21):  #this excludes the frequency ranges and ReIm, which are specificed later, and DelA 
+        if (i not in lagonly_dbl):
             print("Input parameter ",i+1,", ",parameters_dbl[i])
             sim_pars[i] = input()
-    sim_pars[16] = 0.
-    sim_pars[17] = 0.
-    sim_pars[18] = 1
-    sim_pars[19] = 0.
-    sim_pars[20] = 0.
-    sim_pars[21] = 0.
 else:
-    print("Input simulation parameter range for the time-averaged spectrum:")
+    print("Input simulation parameter range for every spectrum:")  
     sim_pars_min = np.zeros(len(parameters_dbl))
     sim_pars_max = np.zeros(len(parameters_dbl))
     sim_pars = np.zeros(len(parameters_dbl))    
     for i in range(len(parameters_dbl)):
-        if (i<16 or i>21):  #this excludes the frequency ranges and ReIm, which are specificed later, and DelA 
+        if (i not in lagonly_dbl):
             print("Input parameter ",i+1,", ",parameters_dbl[i], " min and max values")
             sim_pars_min[i] = input()
             sim_pars_max[i] = input()
             sim_pars[i] = random.uniform(sim_pars_min[i],sim_pars_max[i])
-    sim_pars[16] = 0.
-    sim_pars[17] = 0.
-    sim_pars[18] = 1
-    sim_pars[19] = 0.
-    sim_pars[20] = 0.
-    sim_pars[21] = 0.
+
 save_file ="Parameters/spec_pars.dat"
 save_file = open(save_file, "w") 
 for i in range(len(sim_pars)):
     save_file.write(str(sim_pars[i])+"\n")
 save_file.close()
-#Input flux and exposure time, set up the model spectrum, and run the simulation for the time averaged spectrum
-print("Simulating spectra:")
+
+#sort out parameters for the simulation of both flux and lag energy spectra:
+spec_pars = np.zeros(len(sim_pars)-2)
+for i in range(len(spec_pars)):
+    if (i < 16):
+        spec_pars[i] = sim_pars[i]
+    elif (i == 19 or i == 22 or i == 23):
+        spec_pars[i] = 1.
+    else:
+        spec_pars[i] == sim_pars[len(sim_pars)-1]
+    
 print("Input source flux between 0.5 and 10 keV, in erg/cm^2/s:")
 flux_source = input()
-print("Input exposure:")
-Texp = input()
+
+#Input flux and exposure time, set up the model spectrum, and run the simulation for the time averaged spectrum
+print("Simulating spectra:")
 AllModels.lmod("reltrans",dirPath="~/Software/Reltrans")
-sim_model = Model("rtransDbl",setPars=sim_pars.tolist())
+sim_model = Model("rtransDbl",setPars=spec_pars.tolist())
 AllModels.calcFlux("0.5 10.0")
 flux_model = AllModels(1).flux
 renorm = float(flux_source)/float(flux_model[0])
 sim_model.rtransDbl.norm = renorm
 AllModels.calcFlux("0.5 10.0")
 sim_settings = FakeitSettings(response='~/Data/Response/nicer-rmf6s-teamonly-array50.rmf',arf='~/Data/Response/nicer-consim135p-teamonly-array50.arf',
-                              exposure=Texp,fileName='Products/TimeAveraged_sim.pha')
+                              exposure=sim_pars[len(sim_pars)-3],fileName='Products/TimeAveraged_sim.pha')
 AllData.fakeit(1,sim_settings,applyStats=True,filePrefix="")
 AllData.ignore("**-0.5")
 AllData.ignore("10.0-**")
@@ -119,7 +116,6 @@ ax1.scatter(xVals,yVals,color=plot_color)
 ax1.plot(xVals,modVals,color=mod_color,linewidth=2.5)    
 ax1.set_xscale("log",base=10)
 ax1.set_yscale("log",base=10)
-#plt.xlabel("Energy (keV)")
 ax1.set_ylabel("kev$^{2}$ counts s$^{-1}$ keV$^{-1}$")
 ax1.xaxis.set_major_formatter(plt.NullFormatter())
 #plot residuals
@@ -138,9 +134,38 @@ ax2.set_ylabel("$\chi$")
 plt.tight_layout()
 fig.subplots_adjust(hspace=0)   
 plt.savefig("Plots/Spectrum_fit.pdf")
-
-#now simulate the lag-energy spectra:          
-lag_pars = np.zeros(len(sim_pars))+3
+AllData.clear()
 
 #later: 1)figure out how to do lags - probably from xspec interface?
-#2) figure out different model flavours?
+print("-----------------------------------------------------------------------------------")
+print("Flux energy spectrum done; lag energy spectra left")
+print("Type how many lag-energy spectra you want to simulate")
+lagen_number = input()
+for j in range(int(lagen_number)):
+    if(sim_mode == "value"):
+        print("Input simulation parameters for lag energy spectrum:",j+1) 
+        lag_pars = np.zeros(len(parameters_dbl))
+        for i in range(len(parameters_dbl)):
+            if (i in lagonly_dbl):
+                print("Input parameter ",i+1,", ",parameters_dbl[i])
+                lag_pars[i] = input()
+            else:
+                lag_pars[i] = sim_pars[i] 
+    else:
+        print("Input simulation parameter range for every spectrum:")  
+        lag_pars_min = np.zeros(len(parameters_dbl))
+        lag_pars_max = np.zeros(len(parameters_dbl))
+        lag_pars = np.zeros(len(parameters_dbl))    
+        for i in range(len(parameters_dbl)):
+            print("Input parameter ",i+1,", ",parameters_dbl[i], " min and max values")
+            lag_pars_min[i] = input()
+            lag_pars_max[i] = input()
+            lag_pars[i] = random.uniform(lag_pars_min[i],lag_pars_max[i]) 
+    print("Simulating lags:")
+    #print(lag_pars)
+    sim_model = Model("simrtdbl",setPars=lag_pars.tolist())  
+    #note that all the rest of the timing stuff with paths etc. is handled by the Fortran code
+ 
+#add a script to fucking move the files over, since fortran is god awful with strings
+
+#TBD: figure out different model flavours
